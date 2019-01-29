8b312cb4aeb0e4daa637467af6943baa1f799189
ShaherAhly2005 <46113872+Galaxyserver@users.noreply.github.com>
2019-01-30 00:20:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ const client = new Discord.Client({disableEveryone: true});
 
 const request = require('request');
 
-const prefix = '.'
+const prefix = 'S'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
